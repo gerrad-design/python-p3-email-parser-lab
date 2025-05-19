f5825be591b0693ed142126fb96e1b22c132799d
gerrad <gerrad.meitamei@student.moringaschool.com>
2025-05-20 02:19:08 +0300

    initial commit

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,11 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, addresses):
+        self.addresses = addresses
+
+    def parse(self):
+        # Extract valid email addresses using regex
+        emails = re.findall(r'\b[\w\.-]+@[\w\.-]+\.\w+\b', self.addresses)
+        # Remove duplicates and sort
+        return sorted(set(emails))
